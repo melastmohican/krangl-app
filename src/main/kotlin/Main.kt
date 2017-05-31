88d623408ea617355cd1e6748baed79e51b12b6f
Mariusz Jurgielewicz <mariusz.jurgielewicz@qiagen.com>
2017-05-31 10:46:02 -0700

    Different header and filtered columns

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ import java.io.*
 fun main(args: Array<String>) {
 	println(System.getProperty("user.dir"))
 	val hA: Array<String> = arrayOf("mutation_id", "unspecified_with_phenotype_chr", "unspecified_without_phenotype_chr",
-			"disease_segregates_with_mutation", "functional_study_protein_has_property", "gain_of_function",
+			"disease_segregates_with_mutation", "finding_id", "functional_study_protein_has_property", "gain_of_function",
 			"mutation_property", "acquisition_source_id", "compound_het_with_phenotype",
 			"compound_het_without_phenotype", "het_with_phenotype", "het_without_phenotype",
 			"num_other_complex_with_phenotype", "num_other_complex_without_phenotype", "num_total_het_with_phenotype",
@@ -17,9 +17,8 @@ fun main(args: Array<String>) {
 			"complex_homozygous_with_phenotype", "complex_homozygous_without_phenotype",
 			"other_complex_with_phenotype", "other_complex_without_phenotype")
 	println(hA.size)
-	val hB: Array<String> = arrayOf<String>("mutation_id",
-			"unspecified_with_phenotype_chr", "unspecified_without_phenotype_chr", "disease_segregates_with_mutation",
-			"functional_study_protein_has_property", "gain_of_function", "mutation_property", "acquisition_source_id",
+	val hB: Array<String> = arrayOf<String>("mutation_id", "unspecified_with_phenotype_chr", "unspecified_without_phenotype_chr", "disease_segregates_with_mutation",
+			"finding_id", "functional_study_protein_has_property", "gain_of_function", "mutation_property", "acquisition_source_id",
 			"compound_het_with_phenotype", "compound_het_without_phenotype", "het_with_phenotype",
 			"het_without_phenotype", "num_other_complex_with_phenotype", "num_other_complex_without_phenotype",
 			"num_total_het_with_phenotype", "num_total_het_without_phenotype", "unspecified_with_phenotype",
@@ -31,13 +30,37 @@ fun main(args: Array<String>) {
 			"complex_homozygous_without_phenotype", "other_complex_with_phenotype", "other_complex_without_phenotype")
 	println(hB.size)
 
+	val fA: Array<String> = arrayOf<String>("mutation_id", "unspecified_with_phenotype_chr", "unspecified_without_phenotype_chr",
+			"disease_segregates_with_mutation", "functional_study_protein_has_property", "gain_of_function",
+			"mutation_property", "acquisition_source_id", "compound_het_with_phenotype",
+			"compound_het_without_phenotype", "het_with_phenotype", "het_without_phenotype",
+			"num_other_complex_with_phenotype", "num_other_complex_without_phenotype", "num_total_het_with_phenotype",
+			"num_total_het_without_phenotype", "unspecified_with_phenotype", "unspecified_without_phenotype",
+			"patients_from_single_family", "affected_family_member_lacks_mutation", "primary_reference",
+			"hemizygous_with_phenotype", "hemizygous_without_phenotype", "mut_in_other_gene_with_phenotype",
+			"mut_in_other_gene_without_phenotype", "total_het_with_phenotype", "total_het_without_phenotype",
+			"finding_type", "patients_with_phenotype", "patients_without_phenotype", "denovo", "clinical_significance",
+			"complex_homozygous_with_phenotype", "complex_homozygous_without_phenotype",
+			"other_complex_with_phenotype", "other_complex_without_phenotype")
+	val fB: Array<String> = arrayOf<String>("mutation_id", "unspecified_with_phenotype_chr", "unspecified_without_phenotype_chr", "disease_segregates_with_mutation",
+			"functional_study_protein_has_property", "gain_of_function", "mutation_property", "acquisition_source_id",
+			"compound_het_with_phenotype", "compound_het_without_phenotype", "het_with_phenotype",
+			"het_without_phenotype", "num_other_complex_with_phenotype", "num_other_complex_without_phenotype",
+			"num_total_het_with_phenotype", "num_total_het_without_phenotype", "unspecified_with_phenotype",
+			"unspecified_without_phenotype", "patients_from_single_family", "affected_family_member_lacks_mutation",
+			"primary_reference", "hemizygous_with_phenotype", "hemizygous_without_phenotype",
+			"mut_in_other_gene_with_phenotype", "mut_in_other_gene_without_phenotype", "total_het_with_phenotype",
+			"total_het_without_phenotype", "finding_type", "patients_with_phenotype", "patients_without_phenotype",
+			"denovo", "clinical_significance", "complex_homozygous_with_phenotype",
+			"complex_homozygous_without_phenotype", "other_complex_with_phenotype", "other_complex_without_phenotype")
+
 	val a: DataFrame = DataFrame.fromCSV(File("mutation_property.txt"), format = CSVFormat.TDF.withHeader(*hA))
 	a.glimpse()
 	val b: DataFrame = DataFrame.fromCSV(File("mutation_property_pwcb_test.txt"), format = CSVFormat.newFormat('|').withHeader(*hB))
 	b.glimpse()
 
-	val ga = a.count(*hA)
-	val gb = b.count(*hB)
+	val ga = a.count(*fA)
+	val gb = b.count(*fB)
 	println(ga.rows.toList())
 	println(ga.rows.toList().size)
 	println(gb.rows.toList())
